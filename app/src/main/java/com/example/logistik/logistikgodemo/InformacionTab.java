1d8870a8fc71673e391ad6f3f7ba613250fb1b3c
Jorge Enríquez <jorgeenriquez2014@hotmail.com>
2017-08-08 16:36:25 -0500

    JorgeEnriquez 08/08/2017 : 16:36

--- app/src/main/java/com/example/logistik/logistikgodemo/InformacionTab.java
+++ app/src/main/java/com/example/logistik/logistikgodemo/InformacionTab.java
@@ -3,6 +3,8 @@ package com.example.logistik.logistikgodemo;
 /**
  * Created by Jorge Enríquez y por uriel ;) on 06/08/2017.
  */
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -19,7 +21,17 @@ public class InformacionTab extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_informacion, container, false);
         textView = (TextView)view.findViewById(R.id.txtCantidad);
-        textView.setText("0001");
+//        textView.setText("0001");
+
+        Bundle bundle = getActivity().getIntent().getExtras();
+
+        if (bundle != null)
+            textView.setText(bundle.getString("DATO"));
         return view;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 }
