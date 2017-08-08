aa9a6ee5f9344ffca3a54e1780fea7c5203b6c1e
Jorge Enríquez <jorgeenriquez2014@hotmail.com>
2017-08-08 08:32:17 -0500

    Jorge 08/08/2017 : 08:32

--- app/src/main/java/com/example/logistik/logistikgodemo/ViajeCursoTab.java
+++ app/src/main/java/com/example/logistik/logistikgodemo/ViajeCursoTab.java
@@ -3,7 +3,6 @@ package com.example.logistik.logistikgodemo;
 /**
  * Created by Jorge Enríquez and uriel ;) on 06/08/2017.
  */
-import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,18 +11,31 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class ViajeCursoTab extends Fragment implements Activity_seguimientoViaje.OnFragmentInteractionListener {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ViajeCursoTab extends Fragment implements OnMapReadyCallback{
     Button button;
     TextView textView;
+    GoogleMap mgoogleMap;
+    MapView mapView;
+    View view;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){super.onCreate(savedInstanceState);}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_viaje_curso, container, false);
-        button = (Button)rootView.findViewById(R.id.btn_viaje_curso);
-        textView = (TextView)rootView.findViewById(R.id.text_viaje_curso);
+        view = inflater.inflate(R.layout.fragment_viaje_curso, container, false);
+        button = (Button)view.findViewById(R.id.btn_viaje_curso);
         button.setText("Llegada Origen");
-        textView.setText("Llegada Origen");
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,19 +43,33 @@ public class ViajeCursoTab extends Fragment implements Activity_seguimientoViaje
                 if (button.getText() == "Llegada Origen")
                 {
                     button.setText("Descarga");
-                    textView.setText("Descarga");
                 }
                 else {
                     button.setText("Llegada Origen");
-                    textView.setText("Llegada Origen");
                 }
             }
         });
-        return rootView;
+        return view;
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        mapView = (MapView) view.findViewById(R.id.map);
+        if(mapView != null){
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
     }
 
     @Override
-    public void onFragmentInteraction(Uri uri) {
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
 
+        mgoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(22.1202234, -101.0202731)).title("Logisti-K de México").snippet("Asegurate que suceda"));
+        CameraPosition position = CameraPosition.builder().target(new LatLng(22.1202234, -101.0202731)).zoom(15).bearing(0).tilt(45).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(position));
     }
 }
