1c6e4ff64afa60803346d024c8b3ab3bcaa1f9d6
uriel gaytan <urielgaytanjimenez@gmail.com>
2017-08-10 12:07:35 -0500

    UrielGaytan 10/08/2017 12:08

--- app/src/main/java/com/example/logistik/logistikgodemo/ViajeCursoTab.java
+++ app/src/main/java/com/example/logistik/logistikgodemo/ViajeCursoTab.java
@@ -1,80 +1,144 @@
 package com.example.logistik.logistikgodemo;
 
-/**
- * Created by Jorge Enríquez and uriel ;) on 06/08/2017.
- */
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ViajeCursoTab extends Fragment implements OnMapReadyCallback{
+public class ViajeCursoTab extends Fragment implements OnMapReadyCallback {
     Button button;
-    GoogleMap mgoogleMap;
+    GoogleMap mMap;
     MapView mapView;
     View view;
+    private Marker marcador;
+    double lat = 0.0;
+    double lng = 0.0;
+    double latitud = 0.0;
 
     @Override
-    public void onCreate(Bundle savedInstanceState){super.onCreate(savedInstanceState);}
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_viaje_curso, container, false);
-        button = (Button)view.findViewById(R.id.btn_viaje_curso);
+        button = (Button) view.findViewById(R.id.btn_viaje_curso);
         button.setText("Llegada Origen");
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (button.getText() == "Llegada Origen")
-                {
+                if (button.getText() == "Llegada Origen") {
                     button.setText("Descarga");
-                }
-                else {
+                } else {
                     button.setText("Llegada Origen");
                 }
             }
         });
+        mapView  = (MapView) view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);
         return view;
     }
 
-    public void onViewCreated(View view, Bundle savedInstanceState){
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mapView = (MapView) view.findViewById(R.id.map);
-        if(mapView != null){
-            mapView.onCreate(null);
-            mapView.onResume();
-            mapView.getMapAsync(this);
-        }
+        // mMap = (GoogleMap)view.findViewById(R.id.map);
+//        if(mMap != null){
+//            mMap.getMapAsync(this);
+//        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        MapsInitializer.initialize(getContext());
-        mgoogleMap = googleMap;
+        mMap = googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(22.1202234, -101.0202731)).title("Logisti-K de México").snippet("Asegurate que suceda"));
-        CameraPosition position = CameraPosition.builder().target(new LatLng(22.1202234, -101.0202731)).zoom(15).bearing(0).tilt(45).build();
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(position));
+
+        miUbicacion();
     }
+
+    private void agregarMarcador(double lat, double lng) {
+
+        LatLng coordenadas = new LatLng(lat, lng);
+        CameraUpdate miubicacion = CameraUpdateFactory.newLatLngZoom(coordenadas, 16);
+        if (marcador != null) marcador.remove();
+        marcador = mMap.addMarker(new MarkerOptions().position(coordenadas).title("Mi ubicacion").icon(BitmapDescriptorFactory.fromResource(R.mipmap.trailer2)).flat(true).rotation(4));
+        mMap.animateCamera(miubicacion);
+    }
+
+    private void actualizarUbicacion(Location location) {
+        if (location != null) {
+            lat = location.getLatitude();
+            lng = location.getLongitude();
+            latitud = location.getLatitude();
+            agregarMarcador(lat, lng);
+        }
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            actualizarUbicacion(location);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    private void miUbicacion() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            //return;
+        }
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        actualizarUbicacion(location);
+        locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 1000, 0, locationListener);
+    }
+
+
 
 }
