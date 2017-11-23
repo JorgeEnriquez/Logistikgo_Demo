26dc806efe7ec151014efeeb626dd8e3e044cdfa
Jorge Enríquez <jorgeenriquez2014@hotmail.com>
2017-11-23 09:32:14 -0600

    JorgeEnriquez 23/11/2017 : 09:32

--- app/src/main/java/com/example/logistik/logistikgodemo/ViajeCursoActivity.java
+++ app/src/main/java/com/example/logistik/logistikgodemo/ViajeCursoActivity.java
@@ -158,7 +158,7 @@ public class ViajeCursoActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 2;
+            return 3;
         }
 
         @Override
